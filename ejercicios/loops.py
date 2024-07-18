f9f073c35bb8e784db9a057ddf62c183d0350dbc
Jdrouilly <jose.drouilly@usach.cl>
2024-07-18 15:16:46 -0400

    realizamos un par de ejercicios mas

---
+++ ejercicios/loops.py
@@ -0,0 +1,5 @@
+def loops():
+    printFirstTen = list(map(lambda x: print(x), range(1,11)))
+    
+if __name__=="__main__":
+    loops()
--- ejercicios/selectList.py
+++ ejercicios/selectList.py
@@ -1,3 +1,6 @@
 def showThirdList():
     fruit=["manzana", "platano", "frutilla", "mango", "kiwi"]
-    return print(fruit[2])
+    return print(fruit[1:])
+
+if __name__=="__main__":
+    showThirdList()
